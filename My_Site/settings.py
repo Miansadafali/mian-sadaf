f28af3964063735041d1bd63815f35410402bfe5
Miansadafali <miansadafali@gmail.com>
2023-09-26 12:49:25 -0700

    changes in setting

--- My_Site/settings.py
+++ My_Site/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.enivron('DEBUG', True)
+DEBUG = True
 
-ALLOWED_HOSTS = [os.environ('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = []
 
 
 # Application definition
