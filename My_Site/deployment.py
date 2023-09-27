dccab9bb7c6a90f121b4a10d6be410e35e7a3304
Miansadafali <miansadafali@gmail.com>
2023-09-27 10:40:24 -0700

    getting project ready for heroky

--- My_Site/deployment.py
+++ My_Site/deployment.py
@@ -22,9 +22,6 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ('DATABASE_URL')
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(';')}
-
 DATABASES = {
     'default': dj_database_url.config()
 }
