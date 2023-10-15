8e798708946e525848af25e2dd21775a1d9a034a
Miansadafali <miansadafali@gmail.com>
2023-10-15 04:49:47 -0700

    middleware for www and non www host

--- My_Site/deployment.py
+++ My_Site/deployment.py
@@ -1,18 +1,37 @@
 import os
+
+from django.http import HttpResponsePermanentRedirect
 from .settings import *
 import dj_database_url
+
+
+class WWWRedirectMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        host = request.get_host()
+        if not host.startswith('www.'):
+            new_url = '{}://www.{}{}'.format(
+                'https' if request.is_secure() else 'http',
+                host,
+                request.path
+            )
+            return HttpResponsePermanentRedirect(new_url)
+        return self.get_response(request)
+    
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = [
     'mian-sadaf-093df2897493.herokuapp.com',
-    'miansadaf.tech',
     'www.miansadaf.tech'
 ]
 
 DEBUG = False
 
 MIDDLEWARE = [
+    'My_Site.deployment.WWWRedirectMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
